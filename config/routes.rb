fa0e715695b244f26ee5fb8343e32282e7101bec
Yomari <m1@M1s-MacBook-Air.local>
2022-08-17 22:33:58 +0100

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
