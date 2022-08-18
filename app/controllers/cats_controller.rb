25af44f68ab7ca11de24d47b8457896950003ca5
Yomari <m1@M1s-MacBook-Air.local>
2022-08-18 22:58:23 +0100

    Add show route, action to Display filterd cats by breed

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,8 @@ class CatsController < ApplicationController
   def index
     @pagy, @cats = pagy(Cat.desc_cats)
   end
+
+  def show
+    @cats = Cat.filter_cats_by_breed(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "cats#index"
-  resources :cats, :breeds, only: %i[index destroy]
+  resources :cats, :breeds, only: %i[index destroy show]
   
 end
