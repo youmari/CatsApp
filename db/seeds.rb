c89fb998d602e0d5abb9b67f8f9a1be50fa14e7a
Yomari <m1@M1s-MacBook-Air.local>
2022-08-18 12:29:19 +0100

    Seed the cats from the api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'rest-client'
+require 'rails/configuration'
+
+url = "https://api.thecatapi.com/v1/images/search"
+
+data = JSON.parse(RestClient.get(url, {'x-api-key' => 'd34bb7f3-c194-48d3-9109-b5e22567fd7e', params: {limit: 20, has_breeds: 1}}))
+
+data.each do |cat|
+  unless Breed.find_by(id: cat["breeds"][0]["id"])
+    Breed.create(id: cat["breeds"][0]["id"], name: cat["breeds"][0]["name"], origin: cat["breeds"][0]["origin"])
+  end
+  Cat.create(id: cat['id'], url: cat['url'], width: cat['width'], height: cat['height'], breed_id: cat["breeds"][0]["id"])
+end
