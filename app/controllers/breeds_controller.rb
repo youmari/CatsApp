042ee50682c9fd7e0bb0c5bda4d793f2b5f4dd92
Yomari <m1@M1s-MacBook-Air.local>
2022-08-19 11:46:04 +0100

    Add search to breeds name and create capitalize method

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,6 +1,14 @@
 class BreedsController < ApplicationController
   def index
-    @breeds = Breed.all.includes(:cats).order(:name)
+    @search_breed = Breed.where("name LIKE ?", "%#{capitalize}%") if Breed.find_by("name LIKE ?", "%#{capitalize}%")
+    @breeds = @search_breed || Breed.all.includes(:cats).order(:name)
     @total_count = Breed.total_cats_grouped_by_breed
   end
+
+  private
+  def capitalize
+    if params[:search]
+      serach_word = params[:search].split(' ').each  {|word| word.capitalize! }.join(' ')
+    end
+  end 
 end
